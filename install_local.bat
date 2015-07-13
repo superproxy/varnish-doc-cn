53989ba07c8f5ec0c5152640c2f90dd3c50fa722
superproxy <sharpcoder@qq.com>
2015-07-13 19:16:57 +0800

    添加执行脚本

---
+++ install_local.bat
@@ -0,0 +1,6 @@
+set python=%cd%\build_tools\Python27
+set path=%python%;%python%\Scripts;%path%
+
+pip  install sphinx
+
+echo end
---
+++ run.bat
@@ -0,0 +1,7 @@
+set python=%cd%\build_tools\Python27
+set path=%python%;%python%\Scripts;%path%
+
+ 
+cd ..
+
+make html
